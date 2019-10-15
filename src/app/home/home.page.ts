63646e72ace70e5e9461d70b60cb707b15dd3bb2
Erik Grohnwaldt <erik@grohnwaldt.de>
2019-10-15 23:35:39 +0200

    Beginn Eingabevalidierung

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Automat } from '../models/automat';
 import { Transition } from '../models/transition';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { Transition } from '../models/transition';
 })
 export class HomePage {
   automat: Automat;
-  newTransition : Transition = {
+  newTransition: Transition = {
     state: 'Z',
     input: '',
     stackTop: '#',
@@ -17,96 +18,175 @@ export class HomePage {
     stackOp: '!'
   }
 
-
   /**
    * Validiert die Eingabe gegen das Eingabealphabet und das Kellertopzeichen
-   * @param event 
    */
   validateInput(event) {
-     let result : boolean = (this.automat.alphabetInput.includes( event.key ));
-     result = result || this.automat.stackEmpty == event.key;
-     if (result) {
-       event.target.value = '';
-     }
-     return result;
+    let result: boolean = (this.automat.alphabetInput.includes(event.key));
+    result = result || this.automat.stackEmpty === event.key;
+    if (result) {
+      event.target.value = '';
+    }
+    return result;
   }
 
   /**
    * Validiert gegen das Kelleralphabet und die Operationen
-   * @param event 
    */
   validateOperation(event) {
-    let result : boolean =  (this.automat.alphabetStack.includes( event.key ));
-    result = result || this.automat.opRemove==event.key || this.automat.opNo == event.key;
-    if (result) {
-      event.target.value = '';
-    }
-    return result;
-  }
-  
-  /**
-   * Prüft gegen das Kelleralphabet und das Leer-Symbol
-   * @param event 
-   */
-  validateStackTop(event) {
-    let result : boolean = (this.automat.alphabetStack.includes( event.key ));
-    result =  result || this.automat.stackEmpty == event.key;
+    let result: boolean = (this.automat.alphabetStack.includes(event.key));
+    result = result || this.automat.opRemove === event.key || this.automat.opNo === event.key;
     if (result) {
       event.target.value = '';
     }
     return result;
   }
 
-  getInputArray() {
+  /**
+   * Prüft gegen das Kelleralphabet und das Leer-Symbol
+   */
+  validateStackTop(event) {
+    let result: boolean = (this.automat.alphabetStack.includes(event.key));
+    result = result || this.automat.stackEmpty === event.key;
+    if (result) {
+      event.target.value = '';
+    }
+    return result;
+  }
+
+  /**
+   * Liefert das Inputalphabet als Char-Array
+   */
+  getInputArray(): Array<string> {
     return Array.from(this.automat.input);
   }
 
-  getStackArray() {
+  /**
+   * Liefert das Kelleralphabet als Char-Array
+   */
+  getStackArray(): Array<string> {
     return Array.from(this.automat.stack);
   }
 
-  emptyAutomat() : Automat {
-    return  { alphabetInput: 'abcde', alphabetStack: 'xyz', input: 'acddca', transitions: [], stackEmpty: '#', opNo: "!", opRemove: '?', inputPosition: 0, stack: '#' }; 
+  emptyAutomat(): Automat {
+    return {
+      alphabetInput: 'abcde',
+      alphabetStack: 'xyz',
+      input: 'acddca',
+      transitions: [],
+      stackEmpty: '#',
+      opNo: '!',
+      opRemove: '?',
+      inputPosition: 0,
+      stack: '#'
+    };
   }
 
-  getTransitionString(transition: Transition) {
-    let start : string = '('+transition.state+','+transition.input+','+transition.stackTop+') => (' + transition.stateNext + ',' + transition.stackOp + ')';
-    return start;
+  /**
+   * Liefert das 3=>2-Tupel fuer die Ausgabe
+   * @param string transistion der Zustandsuebergang der ausgegeben werden soll
+   */
+  getTransitionString(transition: Transition): string {
+    return '(' + transition.state + ',' +
+      transition.input + ',' +
+      transition.stackTop +
+      ') => (' +
+      transition.stateNext + ',' +
+      transition.stackOp + ')'
   }
 
   addTransition(transition: Transition) {
     debugger;
     let data = JSON.parse(JSON.stringify(transition));
     this.automat.transitions.push(data);
-    this.automat.transitions.sort((a,b)=>{
+    this.automat.transitions.sort((a, b) => {
       let result: number = a.state.localeCompare(b.state);
-      if (result==0) {
+      if (result === 0) {
         result = a.input.localeCompare(b.input);
       }
-      if (result==0) {
+      if (result === 0) {
         result = a.stackTop.localeCompare(b.stackTop);
       }
       return result;
     });
   }
 
-  checkTransition(transitiion: Transition) {
-    return true;
+  /**
+   * Prueft ob ein Uebergang korrekt erfasst wurde. Falls nicht, wird eine entsprechende Fehlermeldung zurueckgegeben
+   * @param Transition transition Uebergang der geprueft werden soll. 
+   */
+  checkTransition(transition: Transition): string {
+    let result = '';
+    if (transition.state === '') {
+      result = 'Der Startzustand darf nicht leer sein.<br>';
+    }
+    if (transition.input === '') {
+      result = result + 'Das Eingabezeichen darf nicht leer sein.<br>';
+    }
+    if (transition.stackTop === '') {
+      result = result + 'Das Kellertopzeichen darf nicht leer sein<br>';
+    }
+    if (transition.stateNext === '') {
+      result = result + 'Der Zielzustand darf nicht leerein.<br>';
+    }
+    if (transition.stackOp === '') {
+      result = result + 'Die Kelleroperation darf nicht leer sein<br>';
+    }
+    return result;
   }
 
-  openTransition(transition: Transition) { 
+  /**
+   * Gibt es bereits einen Zustandsuebergang mit einer solchen Startkonfiguration?
+   * @param Transition transition 
+   * @return number -1, wenn es einen solchen Zustand nicht gibt
+   */
+  indexOfTransition(transition: Transition): number {
+    let result = -1;
+    let pos = -1;
+    this.automat.transitions.forEach(trans => {
+      pos++;
+      if (transition.input === trans.input && transition.stackTop === trans.stackTop && transition.state === trans.state) {
+        result = pos;
+        return;
+      }
+    });
+    return result;
+  }
+
+  openTransition(transition: Transition) {
     this.newTransition = transition;
   }
 
-  addEntry()  {
-    if (this.checkTransition(this.newTransition)) {
-      this.addTransition(this.newTransition);
+  /**
+   * Fuegt den aktuell bearbeiteten Eintrag hinzu
+   */
+  addEntry() {
+    const checkmessage = this.checkTransition(this.newTransition);
+    if (checkmessage === '') {
+      if (this.indexOfTransition(this.newTransition) > -1) {
+        this.alertCtrl.create({
+          header: 'Doppelte Ausgangskonfiguration',
+          message: 'Es existiert bereits ein Zustandsübergang mit dieser Ausgangssituation - Diesen Zustandsübergang ersetzen?',
+          buttons: ['Ersetzen', 'Abbrechen']
+          // TODO: Handler implementieren
+        }).then(
+          (alert) => alert.present()
+        );
+      } else {
+        this.addTransition(this.newTransition);
+      }
     } else {
-
+      this.alertCtrl.create({
+        message: checkmessage,
+        header: 'Fehler',
+        buttons: ['Ok']
+      }).then((alert) => {
+        alert.present();
+      });
     }
   }
 
-  constructor() {
+  constructor(public alertCtrl: AlertController) {
     this.automat = this.emptyAutomat();
   }
 
